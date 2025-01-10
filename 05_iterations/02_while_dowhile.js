bf8af5920bf4d6586ca2e52e581b5782f74310c8
Tanmay Sakure <87945479+sakuretanmay@users.noreply.github.com>
2025-01-10 18:50:07 +0000

    while and doWhile

---
+++ 05_iterations/02_while_dowhile.js
@@ -0,0 +1,22 @@
+// while loop
+let index = 0;
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2 ;
+}
+
+let myArr = ['fish', 'frog', 'snake']
+
+let i = 0;
+while (i < myArr.length) {
+    // console.log( `value is ${ myArr[i]}`);
+    i++;
+}
+
+// do while
+
+let score = 11;
+do {
+    console.log( `score is ${score}`)
+    score++;
+} while (score <= 10);
