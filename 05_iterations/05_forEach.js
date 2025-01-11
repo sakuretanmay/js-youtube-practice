54989284fd518b6943b4ed7f3a0418f5a6edc563
Tanmay Sakure <87945479+sakuretanmay@users.noreply.github.com>
2025-01-11 19:32:06 +0000

    filter and forEach

---
+++ 05_iterations/05_forEach.js
@@ -0,0 +1,43 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+coding.forEach( function(val){
+//     console.log( "Value in array", val)
+})
+
+coding.forEach( (item) => {
+    // console.log( "each value in array", item)
+})
+
+function printMe( item){
+    // console.log("Printing the item:", item)
+}
+
+coding.forEach(printMe);
+
+const myCoding = [
+    {
+        language: "JavaScript",
+        filename: "js"
+    },
+    {
+        language: "TypeScript",
+        filename: "ts"
+    },
+    {
+        language: "Java",
+        filename: "java"
+    }
+]
+
+myCoding.forEach( (item)=> {
+    // console.log(item.language);
+})
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const values= coding.forEach((item) => {
+    // console.log(item);
+    return item
+})
+
+console.log(values)
+//.forEach() method does not return any value. Its purpose is to iterate over an array and execute a provided callback function for each element
