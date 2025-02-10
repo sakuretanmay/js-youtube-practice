51a2981702886fb2452cdbbabcb01689506b887c
Tanmay Sakure <87945479+sakuretanmay@users.noreply.github.com>
2025-02-10 06:08:16 +0000

    Function call()

--- 10_classes_and_oop/05_call.js
+++ 10_classes_and_oop/05_call.js
@@ -1,14 +1,16 @@
-function setUserName(username){
-    this.username = username;
-    console.log("called")
+function setUserName(username) {
+  this.username = username;
+  console.log("called");
 }
 
-function createUser(username, email, password){
-    setUserName.call(this,username)
+function createUser(username, email, password) {
+  setUserName.call(this, username);
 
-    this.email = email;
-    this.password = password;
+  this.email = email;
+  this.password = password;
 }
 
 const chai = new createUser("tim", "tim@g.com", "123535");
 console.log(chai);
+
+//Calls a method of an object, substituting another object for the current object.
