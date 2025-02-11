4269b6dd584eb72be1547549c41c89e92ad3133a
Tanmay Sakure <87945479+sakuretanmay@users.noreply.github.com>
2025-02-11 17:24:36 +0000

    for each

--- 05_iterations/05_forEach.js
+++ 05_iterations/05_forEach.js
@@ -1,43 +1,43 @@
-const coding = ["js", "ruby", "java", "python", "cpp"]
+const coding = ["js", "ruby", "java", "python", "cpp"];
 
-coding.forEach( function(val){
-//     console.log( "Value in array", val)
-})
+coding.forEach(function (val) {
+  //     console.log( "Value in array", val)
+});
 
-coding.forEach( (item) => {
-    // console.log( "each value in array", item)
-})
+coding.forEach((item) => {
+  // console.log( "each value in array", item)
+});
 
-function printMe( item){
-    // console.log("Printing the item:", item)
+function printMe(item) {
+  // console.log("Printing the item:", item)
 }
 
 coding.forEach(printMe);
 
 const myCoding = [
-    {
-        language: "JavaScript",
-        filename: "js"
-    },
-    {
-        language: "TypeScript",
-        filename: "ts"
-    },
-    {
-        language: "Java",
-        filename: "java"
-    }
-]
+  {
+    language: "JavaScript",
+    filename: "js",
+  },
+  {
+    language: "TypeScript",
+    filename: "ts",
+  },
+  {
+    language: "Java",
+    filename: "java",
+  },
+];
 
-myCoding.forEach( (item)=> {
-    // console.log(item.language);
-})
+myCoding.forEach((item) => {
+  // console.log(item.language);
+});
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-const values= coding.forEach((item) => {
-    // console.log(item);
-    return item
-})
+const values = coding.forEach((item) => {
+  // console.log(item);
+  return item;
+});
 
-console.log(values)
+console.log(values);
 //.forEach() method does not return any value. Its purpose is to iterate over an array and execute a provided callback function for each element
