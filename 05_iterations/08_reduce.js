2a3a6b62aa1a8babab13fba39ccc509a6e8e2bcb
Tanmay Sakure <87945479+sakuretanmay@users.noreply.github.com>
2025-02-13 06:50:00 +0000

    reduce

--- 05_iterations/08_reduce.js
+++ 05_iterations/08_reduce.js
@@ -1,4 +1,4 @@
-const numbers = [ 1, 2, 3, 4];
+const numbers = [1, 2, 3, 4];
 
 // only for understanding
 // const total = numbers.reduce( function(accumulator, currentValue) {
@@ -6,37 +6,36 @@ const numbers = [ 1, 2, 3, 4];
 //     return accumulator + currentValue;
 // }, 0)
 
-const total = numbers.reduce( (acc, curr) => ( acc + curr ), 0);
+const total = numbers.reduce((acc, curr) => acc + curr, 0);
 console.log(total);
 
-
 const shoppingCart = [
-    {
-        course : "js course",
-        price : 999
-    },
-    {
-        course : "java course",
-        price : 2999
-    },
-    {
-        course : "py course",
-        price : 1999
-    },
-    {
-        course : "AI course",
-        price : 5999
-    },
-    {
-        course : "android dev course",
-        price : 3999
-    },
-    {
-        course : "dsa course",
-        price : 4999
-    }
-]
+  {
+    course: "js course",
+    price: 999,
+  },
+  {
+    course: "java course",
+    price: 2999,
+  },
+  {
+    course: "py course",
+    price: 1999,
+  },
+  {
+    course: "AI course",
+    price: 5999,
+  },
+  {
+    course: "android dev course",
+    price: 3999,
+  },
+  {
+    course: "dsa course",
+    price: 4999,
+  },
+];
 
-const cartTotal = shoppingCart.reduce((acc, item)=> acc + item.price, 0);
+const cartTotal = shoppingCart.reduce((acc, item) => acc + item.price, 0);
 
 console.log(cartTotal);
