305c1fc0dfe6bf27434aeb99f1ad52699ef7c23f
Tanmay Sakure <87945479+sakuretanmay@users.noreply.github.com>
2025-02-20 08:26:14 +0000

    basics of js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,15 +1,21 @@
-const accountId = 1234
-let accountEmail = "google@google.com"
-var accountPassword = "134124"
-accountCity = "jaipur"
+const accountId = 1234;
+let accountEmail = "google@google.com";
+var accountPassword = "134124";
+accountCity = "jaipur";
 
 let accountState;
 
 // Prefer not to use var
 // Bcoz of issue in block scoop and functional scoop
 
-accountEmail = "yt@google.com"
-accountPassword = "313131"
-accountCity = "Goa"
+accountEmail = "yt@google.com";
+accountPassword = "313131";
+accountCity = "Goa";
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
